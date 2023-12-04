bc7adc2ffb72c23387e103775b70464d0b5b789f
Nruonan <119746694+Nruonan@users.noreply.github.com>
2023-12-04 15:16:53 +0800

    开发短链接系统 分布式锁注册功能

---
+++ admin/src/main/java/com/xzn/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,9 @@
+package com.xzn.shortlink.admin.common.constant;
+
+/**
+ * @author Nruonan
+ * @description
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
