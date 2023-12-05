1de5205a056ffff4b0bcdc350fc46d420b3edf09
Nruonan <119746694+Nruonan@users.noreply.github.com>
2023-12-05 17:48:42 +0800

    用户登录

---
+++ admin/src/main/java/com/xzn/shortlink/admin/dto/req/UserLoginReqDTO.java
@@ -0,0 +1,16 @@
+package com.xzn.shortlink.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * @author Nruonan
+ *
+ */
+@Data
+public class UserLoginReqDTO {
+    private String username;
+
+    private String password;
+
+
+}
---
+++ admin/src/main/java/com/xzn/shortlink/admin/dto/resp/UserLoginRespDTO.java
@@ -0,0 +1,16 @@
+package com.xzn.shortlink.admin.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Nruonan
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRespDTO {
+    private String token;
+}
