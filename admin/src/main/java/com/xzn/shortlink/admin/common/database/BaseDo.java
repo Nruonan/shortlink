56ff2f390d481d1679d5463c354166075ee32f0e
Nruonan <119746694+Nruonan@users.noreply.github.com>
2023-12-06 12:11:09 +0800

    日期父类

---
+++ admin/src/main/java/com/xzn/shortlink/admin/common/database/BaseDo.java
@@ -0,0 +1,32 @@
+package com.xzn.shortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * @author Nruonan
+ * @description
+ */
+@Data
+public class BaseDo {
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private int delFlag;
+
+}
