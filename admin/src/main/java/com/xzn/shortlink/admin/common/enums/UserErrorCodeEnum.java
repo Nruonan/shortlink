1db56011e006b61637d7752af7c1ca1a7c50f663
Nruonan <15231613@qq.com>
2023-12-19 17:55:25 +0800

    开发短链接项目更新用户登录功能

--- admin/src/main/java/com/xzn/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/xzn/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -7,6 +7,7 @@ import com.xzn.shortlink.admin.common.convention.errorcode.IErrorCode;
  * @description
  */
 public enum UserErrorCodeEnum implements IErrorCode {
+    USER_TOKEN_FAIL("A000200","用户Token验证失败"),
     USER_NULL("B000200","用户记录不存在"),
     USER_NAME_EXIST("B000201","用户姓名已存在"),
     USER_EXIST("B000202","用户记录已存在"),
