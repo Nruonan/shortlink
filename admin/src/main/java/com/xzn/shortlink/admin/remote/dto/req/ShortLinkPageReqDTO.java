92f6c38a89298186b1f0c4bc1fe68c4fae4aa2d6
Nruonan <15231613@qq.com>
2023-12-20 10:27:57 +0800

    开发短链接后管远程调用接口

---
+++ admin/src/main/java/com/xzn/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,17 @@
+package com.xzn.shortlink.admin.remote.dto.req;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+/**
+ * @author Nruonan
+ * @description 短链接分页请求参数
+ */
+@Data
+public class ShortLinkPageReqDTO extends Page{
+    /**
+     * a分页标识
+     */
+    private String gid;
+}
---
+++ admin/src/main/java/com/xzn/shortlink/admin/remote/dto/resp/ShortLinkCreateRespDTO.java
@@ -0,0 +1,35 @@
+package com.xzn.shortlink.admin.remote.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Nruonan
+ * @description 短链接创建响应实体对象
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ShortLinkCreateRespDTO {
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+
+
+}
